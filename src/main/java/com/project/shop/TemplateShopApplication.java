6aa25fe15efb2c64f9db9c4d28f09170843dd2fd
SirPapelas <joseantonioruizjm@gmail.com>
2023-12-24 00:29:36 +0100

    First commit

---
+++ src/main/java/com/project/shop/TemplateShopApplication.java
@@ -0,0 +1,13 @@
+package com.project.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateShopApplication.class, args);
+	}
+
+}
